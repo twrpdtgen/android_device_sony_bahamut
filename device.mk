c39c1f9a31362a9c75c27eca3f315461cc816b8c
plente00 <123705363+plente00@users.noreply.github.com>
2023-03-04 21:51:36 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,13 +19,11 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.msmnile
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.msmnile \
     libgptutils \
     libz \
     libcutils
+
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -33,3 +31,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+    
+
+
